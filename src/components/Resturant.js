6eecdf84861fda6abfe116a1d2e52731bc4188d6
ANKUSH Banerjeee <banerjeeankush184@gmail.com>
2022-06-18 01:36:58 +0530

    frontend done

--- src/components/Resturant.js
+++ src/components/Resturant.js
@@ -2,9 +2,17 @@ import React, { useState } from 'react'
 import "../style.css"
 import Menu from './Menuapi'
 import Menucard from './Menucard'
+import Navbar from './Navbar'
+const uniquelist =[
+    ...new Set(Menu.map((curr)=>{
+        return curr.category
+    }))
+]
 function Resturant() {
    const [menudata,setmenudata ] = useState(Menu)
+   const[menulist,setmenulist]=useState(uniquelist)
    const fileritem=(category)=>{
+
 const updatedlist =Menu.filter((curr)=>{
     return curr.category===category;
 })
@@ -12,17 +20,8 @@ setmenudata(updatedlist)
    }
     return (
       <>
-      <nav className='navbar'>
-        <div className='btn-group'>
-            <button className='btn-group__item' onClick={()=>fileritem("breakfast")}>breakfast</button>
-            <button className='btn-group__item'  onClick={()=>fileritem("lunch")}>lunch</button>
-            <button className='btn-group__item'onClick={()=>fileritem("evening")}>evening</button>
-            <button className='btn-group__item'onClick={()=>fileritem("dinner")}>dinner</button>
-            <button className='btn-group__item'onClick={()=>setmenudata(Menu)}>All</button>
-
-        </div>
-      </nav>
-
+     
+        <Navbar filterItem={fileritem} menuList={menulist}/>
         <Menucard data={menudata}/>
 
       </>
